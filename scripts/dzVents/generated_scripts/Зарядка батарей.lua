693756f53f79477f0ad2791656902a2d8fac004e
CobraFX <cobrafx@gmail.com>
2024-10-04 11:15:58 +0300

    Поправив Зарядку Батареї

--- scripts/dzVents/generated_scripts/Зарядка батарей.lua
+++ scripts/dzVents/generated_scripts/Зарядка батарей.lua
@@ -4,30 +4,31 @@ return {
             'every 1 minutes'
       },
       devices = {
-         "Контакт розетки котла"
+         "Контакт розетки котла",
+         "BMS JK - Battery SoC"
       }
    },
    execute = function(domoticz, switch)
 
       local _u = domoticz.utils
       
-      if _u.deviceExists('Powmr - Active Max Utility Charging') and _u.deviceExists('Контакт розетки котла') and _u.deviceExists('Powmr - Max Utility Charging') then
+      if _u.deviceExists('Powmr - Active Max Utility Charging') and _u.deviceExists('Контакт розетки котла') and _u.deviceExists('Powmr - Max Utility Charging') and _u.deviceExists('BMS JK - Battery SoC') then
           local kotel = domoticz.devices('Контакт розетки котла')
           local max_utility_charging = domoticz.devices('Powmr - Active Max Utility Charging')
           local utility_charging = domoticz.devices('Powmr - Max Utility Charging')
-          
-        --   local msg = "State: " .. utility_charging.sValue
-        --   domoticz.notify('Powmr', msg, domoticz.PRIORITY_HIGH)
-          
+          local bms_soc = domoticz.devices('BMS JK - Battery SoC')
+          local soc_value = tonumber(bms_soc.sValue)
+          local current_value = tonumber(utility_charging.sValue)
+
           if(kotel.state == "On") then
-            if(utility_charging.sValue ~= "10.0") then
+            if(soc_value < 100 and current_value ~= 10) then
                 max_utility_charging.switchOff()
-                local msg = "Переведено зарядку акумуляторів на 10A!"
+                local msg = "Переведено зарядку акумуляторів (SOC " .. bms_soc.sValue .. "%) на 10A!"
                 domoticz.notify('Powmr', msg, domoticz.PRIORITY_HIGH)
             end
           else
-            if(utility_charging.sValue ~= "90.0") then
-                local msg = "Переведено зарядку акумуляторів на 90A!"
+            if(soc_value < 100 and current_value ~= 90) then
+                local msg = "Переведено зарядку акумуляторів (SOC " .. bms_soc.sValue .. "%) на 90A!"
                 domoticz.notify('Powmr', msg, domoticz.PRIORITY_HIGH)
                 max_utility_charging.switchOn()
             end
